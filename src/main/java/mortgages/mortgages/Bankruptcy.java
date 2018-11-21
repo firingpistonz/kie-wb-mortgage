654c4ce94250503798433e3003ae65e584e006c9
admin <>
2018-11-21 15:15:53 -0500

    {/src/main/java/mortgages/mortgages/Bankruptcy.java}

--- src/main/java/mortgages/mortgages/Bankruptcy.java
+++ src/main/java/mortgages/mortgages/Bankruptcy.java
@@ -16,37 +16,57 @@
 
 package mortgages.mortgages;
 
-public class Bankruptcy extends java.lang.Object implements java.io.Serializable {
+public class Bankruptcy extends java.lang.Object
+		implements
+			java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    @org.kie.api.definition.type.Position(value = 0)
-    private java.lang.Integer amountOwed;
+	@org.kie.api.definition.type.Position(0)
+	private java.lang.Integer amountOwed;
 
-    @org.kie.api.definition.type.Position(value = 1)
-    private java.lang.Integer yearOfOccurrence;
+	@org.kie.api.definition.type.Position(1)
+	private java.lang.Integer yearOfOccurrence;
 
-    public Bankruptcy() {
-    }
+	private float totalAmount;
 
-    public Bankruptcy( java.lang.Integer amountOwed, java.lang.Integer yearOfOccurrence ) {
-        this.amountOwed = amountOwed;
-        this.yearOfOccurrence = yearOfOccurrence;
-    }
+	public Bankruptcy() {
+	}
 
-    public java.lang.Integer getAmountOwed() {
-        return this.amountOwed;
-    }
+	public java.lang.Integer getAmountOwed() {
+		return this.amountOwed;
+	}
 
-    public void setAmountOwed( java.lang.Integer amountOwed ) {
-        this.amountOwed = amountOwed;
-    }
+	public void setAmountOwed(java.lang.Integer amountOwed) {
+		this.amountOwed = amountOwed;
+	}
 
-    public java.lang.Integer getYearOfOccurrence() {
-        return this.yearOfOccurrence;
-    }
+	public java.lang.Integer getYearOfOccurrence() {
+		return this.yearOfOccurrence;
+	}
 
-    public void setYearOfOccurrence( java.lang.Integer yearOfOccurrence ) {
-        this.yearOfOccurrence = yearOfOccurrence;
-    }
+	public void setYearOfOccurrence(java.lang.Integer yearOfOccurrence) {
+		this.yearOfOccurrence = yearOfOccurrence;
+	}
+
+	public float getTotalAmount() {
+		return this.totalAmount;
+	}
+
+	public void setTotalAmount(float totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+
+	public Bankruptcy(java.lang.Integer amountOwed,
+			java.lang.Integer yearOfOccurrence, float totalAmount) {
+		this.amountOwed = amountOwed;
+		this.yearOfOccurrence = yearOfOccurrence;
+		this.totalAmount = totalAmount;
+	}
+
+	public Bankruptcy(java.lang.Integer amountOwed,
+			java.lang.Integer yearOfOccurrence) {
+		this.amountOwed = amountOwed;
+		this.yearOfOccurrence = yearOfOccurrence;
+	}
 }
